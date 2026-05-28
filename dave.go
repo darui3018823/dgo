bwmarrin/discordgo

Add E2EE (DAVE) Support

dave.go
@@ -1,0 +1,240 @@
+package discordgo
+
+import (
+	"crypto/cipher"
+	"encoding/binary"
+	"fmt"
+	"strconv"
+	"sync"
+)
+
+type DAVESession struct {
+	mu                  sync.Mutex
+	protocolVersion     int
+	epoch               uint64
+	pendingTransitionID uint16
+	pendingVersion      int
+
+	exporterSecret    []byte
+	senderKey         []byte
+	senderNonce       uint32
+	frameCipher       cipher.AEAD
+	userID            string
+	active            bool
+	ratchetBaseSecret []byte
+	currentGeneration uint32
+	hasPendingKey     bool
+
+	kpBundle *mlsKeyPackageBundle
+}
+
+func NewDAVESession(userID string) *DAVESession {
+	return &DAVESession{
+		userID: userID,
+	}
+}
+
+func (d *DAVESession) GenerateKeyPackage() ([]byte, error) {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	return d.generateKeyPackageLocked()
+}
+
+func (d *DAVESession) ResetForReWelcome() ([]byte, error) {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	d.active = false
+	d.senderKey = nil
+	d.frameCipher = nil
+	d.exporterSecret = nil
+	d.ratchetBaseSecret = nil
+	d.currentGeneration = 0
+	d.hasPendingKey = false
+
+	return d.generateKeyPackageLocked()
+}
+
+func (d *DAVESession) generateKeyPackageLocked() ([]byte, error) {
+	bundle, err := mlsGenerateKeyPackage(d.userID)
+	if err != nil {
+		return nil, fmt.Errorf("generating key package: %w", err)
+	}
+	d.kpBundle = bundle
+	return bundle.serialized, nil
+}
+
+func (d *DAVESession) HandleExternalSenderPackage(data []byte) error {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	return nil
+}
+
+func (d *DAVESession) HandleWelcome(data []byte) error {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	if d.kpBundle == nil {
+		return fmt.Errorf("no key package generated")
+	}
+
+	result, err := mlsProcessWelcome(data, d.kpBundle)
+	if err != nil {
+		return fmt.Errorf("processing welcome: %w", err)
+	}
+
+	d.exporterSecret = result.exporterSecret
+	d.epoch = result.epoch
+	d.hasPendingKey = true
+	return nil
+}
+
+func (d *DAVESession) HandleCommit(data []byte) error {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	return nil
+}
+
+func (d *DAVESession) HandlePrepareTransition(transitionID uint16, protocolVersion int) {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	d.pendingTransitionID = transitionID
+	d.pendingVersion = protocolVersion
+}
+
+func (d *DAVESession) HandleExecuteTransition(transitionID uint16) error {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	if transitionID != d.pendingTransitionID {
+		if d.senderKey != nil {
+			d.active = true
+		}
+		return nil
+	}
+
+	if d.pendingVersion > 0 {
+		if d.hasPendingKey && d.exporterSecret != nil {
+			if err := d.deriveSenderKeyLocked(); err != nil {
+				return err
+			}
+			d.hasPendingKey = false
+		}
+		if d.senderKey == nil {
+			return nil
+		}
+		d.active = true
+	} else {
+		d.active = false
+		d.senderKey = nil
+		d.frameCipher = nil
+		d.hasPendingKey = false
+	}
+	return nil
+}
+
+func (d *DAVESession) HandlePrepareEpoch(epoch uint64, protocolVersion int) ([]byte, error) {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	d.epoch = epoch
+	d.active = false
+	d.senderKey = nil
+	d.frameCipher = nil
+	d.exporterSecret = nil
+
+	return d.generateKeyPackageLocked()
+}
+
+func (d *DAVESession) DeriveSenderKey() error {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	return d.deriveSenderKeyLocked()
+}
+
+func (d *DAVESession) deriveSenderKeyLocked() error {
+	if d.exporterSecret == nil {
+		return fmt.Errorf("no exporter secret")
+	}
+
+	userIDNum, err := strconv.ParseUint(d.userID, 10, 64)
+	if err != nil {
+		return fmt.Errorf("parsing user ID: %w", err)
+	}
+	context := make([]byte, 8)
+	binary.LittleEndian.PutUint64(context, userIDNum)
+
+	baseSecret, err := mlsExport(d.exporterSecret, daveExportLabel, context, daveKeySize)
+	if err != nil {
+		return fmt.Errorf("exporting base secret: %w", err)
+	}
+
+	d.ratchetBaseSecret = baseSecret
+	d.currentGeneration = 0
+	d.senderNonce = 0
+
+	key, err := hashRatchetGetKey(baseSecret, 0)
+	if err != nil {
+		return fmt.Errorf("deriving ratchet key: %w", err)
+	}
+	d.senderKey = key
+
+	frameCipher, err := newDAVECipher(key)
+	if err != nil {
+		return fmt.Errorf("creating frame cipher: %w", err)
+	}
+	d.frameCipher = frameCipher
+	return nil
+}
+
+func (d *DAVESession) EncryptFrame(opusData []byte) ([]byte, error) {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	if d.frameCipher == nil {
+		return nil, fmt.Errorf("no frame cipher")
+	}
+
+	d.senderNonce++
+
+	generation := d.senderNonce >> 24
+	if generation != d.currentGeneration {
+		d.currentGeneration = generation
+		key, err := hashRatchetGetKey(d.ratchetBaseSecret, generation)
+		if err != nil {
+			return nil, fmt.Errorf("ratcheting key for generation %d: %w", generation, err)
+		}
+		d.senderKey = key
+		frameCipher, err := newDAVECipher(key)
+		if err != nil {
+			return nil, fmt.Errorf("creating cipher for generation %d: %w", generation, err)
+		}
+		d.frameCipher = frameCipher
+	}
+
+	encrypted := encryptSecureFrame(d.frameCipher, d.senderNonce, opusData)
+	return encrypted, nil
+}
+
+func (d *DAVESession) IsActive() bool {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	return d.active
+}
+
+func (d *DAVESession) Reset() {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	d.exporterSecret = nil
+	d.senderKey = nil
+	d.senderNonce = 0
+	d.frameCipher = nil
+	d.active = false
+	d.kpBundle = nil
+	d.pendingTransitionID = 0
+	d.pendingVersion = 0
+	d.ratchetBaseSecret = nil
+	d.currentGeneration = 0
+	d.hasPendingKey = false
+}

dave_crypto.go
@@ -1,0 +1,80 @@
+package discordgo
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"encoding/binary"
+)
+
+const (
+	daveTagSize     = 8
+	daveKeySize     = 16
+	daveExportLabel = "Discord Secure Frames v0"
+)
+
+func encryptSecureFrame(frameCipher cipher.AEAD, nonce uint32, opusData []byte) []byte {
+	fullNonce := buildNonce(nonce)
+	sealed := frameCipher.Seal(nil, fullNonce, opusData, nil)
+
+	ciphertext := sealed[:len(opusData)]
+	fullTag := sealed[len(opusData):]
+	truncatedTag := fullTag[:daveTagSize]
+
+	nonceBytes := encodeULEB128(nonce)
+
+	supplementalSize := byte(daveTagSize + len(nonceBytes) + 0 + 1 + 2)
+
+	result := make([]byte, 0, len(ciphertext)+daveTagSize+len(nonceBytes)+3)
+	result = append(result, ciphertext...)
+	result = append(result, truncatedTag...)
+	result = append(result, nonceBytes...)
+	result = append(result, supplementalSize)
+	result = append(result, 0xFA, 0xFA)
+	return result
+}
+
+func buildNonce(counter uint32) []byte {
+	nonce := make([]byte, 12)
+	binary.LittleEndian.PutUint32(nonce[8:], counter)
+	return nonce
+}
+
+func encodeULEB128(value uint32) []byte {
+	if value == 0 {
+		return []byte{0}
+	}
+	var result []byte
+	for value > 0 {
+		b := byte(value & 0x7F)
+		value >>= 7
+		if value > 0 {
+			b |= 0x80
+		}
+		result = append(result, b)
+	}
+	return result
+}
+
+func newDAVECipher(key []byte) (cipher.AEAD, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	return cipher.NewGCM(block)
+}
+
+func hashRatchetGetKey(baseSecret []byte, generation uint32) ([]byte, error) {
+	secret := baseSecret
+	for i := uint32(0); i < generation; i++ {
+		genCtx := make([]byte, 4)
+		binary.BigEndian.PutUint32(genCtx, i)
+		next, err := mlsExpandWithLabel(secret, "secret", genCtx, 32)
+		if err != nil {
+			return nil, err
+		}
+		secret = next
+	}
+	genCtx := make([]byte, 4)
+	binary.BigEndian.PutUint32(genCtx, generation)
+	return mlsExpandWithLabel(secret, "key", genCtx, 16)
+}

darui3018823/dgo

(cherry picked from commit fd09989565b331268e9719bb059955142c0190bc)

dave.go
@@ -1,0 +1,240 @@
+package dgo
+
+import (
+	"crypto/cipher"
+	"encoding/binary"
+	"fmt"
+	"strconv"
+	"sync"
+)
+
+type DAVESession struct {
+	mu                  sync.Mutex
+	protocolVersion     int
+	epoch               uint64
+	pendingTransitionID uint16
+	pendingVersion      int
+
+	exporterSecret    []byte
+	senderKey         []byte
+	senderNonce       uint32
+	frameCipher       cipher.AEAD
+	userID            string
+	active            bool
+	ratchetBaseSecret []byte
+	currentGeneration uint32
+	hasPendingKey     bool
+
+	kpBundle *mlsKeyPackageBundle
+}
+
+func NewDAVESession(userID string) *DAVESession {
+	return &DAVESession{
+		userID: userID,
+	}
+}
+
+func (d *DAVESession) GenerateKeyPackage() ([]byte, error) {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	return d.generateKeyPackageLocked()
+}
+
+func (d *DAVESession) ResetForReWelcome() ([]byte, error) {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	d.active = false
+	d.senderKey = nil
+	d.frameCipher = nil
+	d.exporterSecret = nil
+	d.ratchetBaseSecret = nil
+	d.currentGeneration = 0
+	d.hasPendingKey = false
+
+	return d.generateKeyPackageLocked()
+}
+
+func (d *DAVESession) generateKeyPackageLocked() ([]byte, error) {
+	bundle, err := mlsGenerateKeyPackage(d.userID)
+	if err != nil {
+		return nil, fmt.Errorf("generating key package: %w", err)
+	}
+	d.kpBundle = bundle
+	return bundle.serialized, nil
+}
+
+func (d *DAVESession) HandleExternalSenderPackage(data []byte) error {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	return nil
+}
+
+func (d *DAVESession) HandleWelcome(data []byte) error {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	if d.kpBundle == nil {
+		return fmt.Errorf("no key package generated")
+	}
+
+	result, err := mlsProcessWelcome(data, d.kpBundle)
+	if err != nil {
+		return fmt.Errorf("processing welcome: %w", err)
+	}
+
+	d.exporterSecret = result.exporterSecret
+	d.epoch = result.epoch
+	d.hasPendingKey = true
+	return nil
+}
+
+func (d *DAVESession) HandleCommit(data []byte) error {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	return nil
+}
+
+func (d *DAVESession) HandlePrepareTransition(transitionID uint16, protocolVersion int) {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	d.pendingTransitionID = transitionID
+	d.pendingVersion = protocolVersion
+}
+
+func (d *DAVESession) HandleExecuteTransition(transitionID uint16) error {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	if transitionID != d.pendingTransitionID {
+		if d.senderKey != nil {
+			d.active = true
+		}
+		return nil
+	}
+
+	if d.pendingVersion > 0 {
+		if d.hasPendingKey && d.exporterSecret != nil {
+			if err := d.deriveSenderKeyLocked(); err != nil {
+				return err
+			}
+			d.hasPendingKey = false
+		}
+		if d.senderKey == nil {
+			return nil
+		}
+		d.active = true
+	} else {
+		d.active = false
+		d.senderKey = nil
+		d.frameCipher = nil
+		d.hasPendingKey = false
+	}
+	return nil
+}
+
+func (d *DAVESession) HandlePrepareEpoch(epoch uint64, protocolVersion int) ([]byte, error) {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	d.epoch = epoch
+	d.active = false
+	d.senderKey = nil
+	d.frameCipher = nil
+	d.exporterSecret = nil
+
+	return d.generateKeyPackageLocked()
+}
+
+func (d *DAVESession) DeriveSenderKey() error {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	return d.deriveSenderKeyLocked()
+}
+
+func (d *DAVESession) deriveSenderKeyLocked() error {
+	if d.exporterSecret == nil {
+		return fmt.Errorf("no exporter secret")
+	}
+
+	userIDNum, err := strconv.ParseUint(d.userID, 10, 64)
+	if err != nil {
+		return fmt.Errorf("parsing user ID: %w", err)
+	}
+	context := make([]byte, 8)
+	binary.LittleEndian.PutUint64(context, userIDNum)
+
+	baseSecret, err := mlsExport(d.exporterSecret, daveExportLabel, context, daveKeySize)
+	if err != nil {
+		return fmt.Errorf("exporting base secret: %w", err)
+	}
+
+	d.ratchetBaseSecret = baseSecret
+	d.currentGeneration = 0
+	d.senderNonce = 0
+
+	key, err := hashRatchetGetKey(baseSecret, 0)
+	if err != nil {
+		return fmt.Errorf("deriving ratchet key: %w", err)
+	}
+	d.senderKey = key
+
+	frameCipher, err := newDAVECipher(key)
+	if err != nil {
+		return fmt.Errorf("creating frame cipher: %w", err)
+	}
+	d.frameCipher = frameCipher
+	return nil
+}
+
+func (d *DAVESession) EncryptFrame(opusData []byte) ([]byte, error) {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	if d.frameCipher == nil {
+		return nil, fmt.Errorf("no frame cipher")
+	}
+
+	d.senderNonce++
+
+	generation := d.senderNonce >> 24
+	if generation != d.currentGeneration {
+		d.currentGeneration = generation
+		key, err := hashRatchetGetKey(d.ratchetBaseSecret, generation)
+		if err != nil {
+			return nil, fmt.Errorf("ratcheting key for generation %d: %w", generation, err)
+		}
+		d.senderKey = key
+		frameCipher, err := newDAVECipher(key)
+		if err != nil {
+			return nil, fmt.Errorf("creating cipher for generation %d: %w", generation, err)
+		}
+		d.frameCipher = frameCipher
+	}
+
+	encrypted := encryptSecureFrame(d.frameCipher, d.senderNonce, opusData)
+	return encrypted, nil
+}
+
+func (d *DAVESession) IsActive() bool {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	return d.active
+}
+
+func (d *DAVESession) Reset() {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	d.exporterSecret = nil
+	d.senderKey = nil
+	d.senderNonce = 0
+	d.frameCipher = nil
+	d.active = false
+	d.kpBundle = nil
+	d.pendingTransitionID = 0
+	d.pendingVersion = 0
+	d.ratchetBaseSecret = nil
+	d.currentGeneration = 0
+	d.hasPendingKey = false
+}

dave_crypto.go
@@ -1,0 +1,80 @@
+package dgo
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"encoding/binary"
+)
+
+const (
+	daveTagSize     = 8
+	daveKeySize     = 16
+	daveExportLabel = "Discord Secure Frames v0"
+)
+
+func encryptSecureFrame(frameCipher cipher.AEAD, nonce uint32, opusData []byte) []byte {
+	fullNonce := buildNonce(nonce)
+	sealed := frameCipher.Seal(nil, fullNonce, opusData, nil)
+
+	ciphertext := sealed[:len(opusData)]
+	fullTag := sealed[len(opusData):]
+	truncatedTag := fullTag[:daveTagSize]
+
+	nonceBytes := encodeULEB128(nonce)
+
+	supplementalSize := byte(daveTagSize + len(nonceBytes) + 0 + 1 + 2)
+
+	result := make([]byte, 0, len(ciphertext)+daveTagSize+len(nonceBytes)+3)
+	result = append(result, ciphertext...)
+	result = append(result, truncatedTag...)
+	result = append(result, nonceBytes...)
+	result = append(result, supplementalSize)
+	result = append(result, 0xFA, 0xFA)
+	return result
+}
+
+func buildNonce(counter uint32) []byte {
+	nonce := make([]byte, 12)
+	binary.LittleEndian.PutUint32(nonce[8:], counter)
+	return nonce
+}
+
+func encodeULEB128(value uint32) []byte {
+	if value == 0 {
+		return []byte{0}
+	}
+	var result []byte
+	for value > 0 {
+		b := byte(value & 0x7F)
+		value >>= 7
+		if value > 0 {
+			b |= 0x80
+		}
+		result = append(result, b)
+	}
+	return result
+}
+
+func newDAVECipher(key []byte) (cipher.AEAD, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	return cipher.NewGCM(block)
+}
+
+func hashRatchetGetKey(baseSecret []byte, generation uint32) ([]byte, error) {
+	secret := baseSecret
+	for i := uint32(0); i < generation; i++ {
+		genCtx := make([]byte, 4)
+		binary.BigEndian.PutUint32(genCtx, i)
+		next, err := mlsExpandWithLabel(secret, "secret", genCtx, 32)
+		if err != nil {
+			return nil, err
+		}
+		secret = next
+	}
+	genCtx := make([]byte, 4)
+	binary.BigEndian.PutUint32(genCtx, generation)
+	return mlsExpandWithLabel(secret, "key", genCtx, 16)
+}